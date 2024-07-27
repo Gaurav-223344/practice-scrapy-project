91d71f96c9fee8065980d55b9c5919d5a954c61d
gaurav-gaikwad-12071998 <122888092+gaurav-gaikwad-12071998@users.noreply.github.com>
2024-07-27 18:55:38 +0530

    added images and crawing 5 pages

--- basic_scrapy/onthemarket.py
+++ basic_scrapy/onthemarket.py
@@ -12,20 +12,29 @@ class OnTheMarket(scrapy.Spider):
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
     }
 
+    custom_settings = {
+        'FEED_FORMAT': 'csv',
+        'FEED_URI': 'basic_scrapy/onthemarket.csv',
+        'LOG_FILE': 'basic_scrapy/onthemarket.log'
+    }
+
     def start_requests(self) -> Iterable[scrapy.Request]:
-        url = 'https://www.onthemarket.com/for-sale/property/abbey-gate/'
-        yield scrapy.Request(url=url, headers=self.headers, callback=self.parse)
+        url = 'https://www.onthemarket.com/for-sale/property/uk'
+        
+        for i in range(1, 6):
+            next_url = url + '/?page=' + str(i)
+            yield scrapy.Request(url=next_url, headers=self.headers, callback=self.parse)
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        html = ''
-        # self.log(response.status)
+        # html = ''
+        # # self.log(response.status)
 
-        with open('template.html', 'r', encoding='utf-8') as html_file:
-            # html_file.write(response.text)
-            for line in html_file.read():
-                html += line
+        # with open('template.html', 'r', encoding='utf-8') as html_file:
+        #     # html_file.write(response.text)
+        #     for line in html_file.read():
+        #         html += line
 
-        response = Selector(text=html) 
+        # response = Selector(text=html) 
 
         for card in response.css('li.otm-PropertyCard'):
             items = {
@@ -33,18 +42,20 @@ class OnTheMarket(scrapy.Spider):
                 "address" : card.css('span.address').css('a::text').get(),
                 "description": [ item.css("li::text").get() for item in card.css('li.otm-ListItemOtmBullet') ],
                 "price": card.css('div.otm-Price').css('div.price::text').get().encode('ascii', 'ignore').decode('utf-8').strip(),
-                "contact": card.css('div.otm-Telephone::text').get()
+                "contact": card.css('div.otm-Telephone::text').get(),
+                "images" : [item.css('img::attr(src)').get() for item in  card.css('div.image-wrapper')]
             }
 
-            print(json.dumps(items, indent=2))
+            yield items
+            # print(json.dumps(items, indent=2))
         # print(response.css('li.otm-PropertyCard'))
         # print(response.xpath('//li[@class="otm-PropertyCard"]/text()'))
 
 
 if __name__ == '__main__':
-    process = OnTheMarket()
-    process.parse("")
+    # process = OnTheMarket()
+    # process.parse("")
 
-    # process = CrawlerProcess()
-    # process.crawl(OnTheMarket)
-    # process.start()
+    process = CrawlerProcess()
+    process.crawl(OnTheMarket)
+    process.start()
